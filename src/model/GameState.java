888ae24f3853dbf3ee0a25e0f458348c45a51a41
Jannick van Ballegooijen <jvbdesignz@gmail.com>
2015-05-21 10:33:09 +0200

    Initial commit "setup"

---
+++ src/model/GameState.java
@@ -0,0 +1,20 @@
+package model;
+
+import java.awt.Graphics2D;
+import java.awt.event.KeyEvent;
+
+import control.ControlManager;
+
+public abstract class GameState {
+	
+	protected ControlManager cm;
+	
+	public GameState(ControlManager cm) {
+		this.cm = cm;
+	}
+	public abstract void draw(Graphics2D g2);
+	public abstract void update();
+	public abstract void init();
+	public abstract void keyPressed(KeyEvent e);
+	public abstract void keyReleased(KeyEvent e);
+}
