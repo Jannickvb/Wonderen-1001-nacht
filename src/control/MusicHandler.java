e173c52f5f1dafd595d91469400811db5e4f133b
Supermaniac101 <Timschijvenaars@hotmail.com>
2015-05-23 18:18:17 +0200

    Added AudioHandler
    For music playback

---
+++ src/control/MusicHandler.java
@@ -0,0 +1,48 @@
+package control;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+
+import view.Main;
+
+public class MusicHandler {
+
+public static ArrayList<AudioInputStream> audio = new ArrayList<AudioInputStream>();
+ Clip clip;
+	
+ 	static{
+		try{
+			audio.add(AudioSystem.getAudioInputStream(Main.class.getResource("/audio/Intro.aiff")));
+			audio.add(AudioSystem.getAudioInputStream(Main.class.getResource("/audio/Poor.aiff")));
+			audio.add(AudioSystem.getAudioInputStream(Main.class.getResource("/audio/Rich.aiff")));
+			audio.add(AudioSystem.getAudioInputStream(Main.class.getResource("/audio/End.aiff")));
+		} catch(Exception ex) {
+	        System.out.println("Error with playing sound.");
+	        ex.printStackTrace();
+	    }
+	}
+ 
+	public MusicHandler() {
+		
+	}
+	
+	public void playSound(AudioType name) throws LineUnavailableException, IOException{
+		this.clip = AudioSystem.getClip();
+		this.clip.open(audio.get(name.ordinal()));
+		this.clip.start();
+	}
+	
+	public void stopSound(){
+		this.clip.stop();
+	}
+	
+	public enum AudioType{
+		intro, poor, rich, end
+	}
+	
+}
--- src/control/VideoHandler.java
+++ src/control/VideoHandler.java
@@ -1,5 +1,7 @@
 package control;
 
+import java.io.File;
+
 import javax.swing.JFrame;
 
 import com.sun.jna.NativeLibrary;
@@ -13,21 +15,22 @@ public class VideoHandler {
 	private JFrame ourFrame = new JFrame();
 	private EmbeddedMediaPlayerComponent ourMediaPlayer;
 	private String mediaPath = "";
+	private File ourFile;
 	
-	VideoHandler(String mediaURL){
-		this.mediaPath = mediaURL;
+	
+	public VideoHandler(File file){
+		this.ourFile = file;
+		mediaPath = ourFile.getAbsolutePath();
 		NativeLibrary.addSearchPath(RuntimeUtil.getLibVlcLibraryName(), "C:/Program Files/VideoLAN/VLC");
 		ourMediaPlayer = new EmbeddedMediaPlayerComponent();
 		ourFrame.setContentPane(ourMediaPlayer);
 		ourFrame.setSize(1200,800);
 		ourFrame.setVisible(true);
 		ourFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		System.out.println(mediaPath);
+		
 	}
 	
 	public void run(){
 		ourMediaPlayer.getMediaPlayer().playMedia(mediaPath);
 	}
-	
-	
 }
