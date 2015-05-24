c41841719918a70b7f3e1eb6537d5f100ce34d14
kees2125 <kpzijlma@avans.nl>
2015-05-24 14:21:34 +0200

    tilemap

--- src/model/tileset/TileMap.java
+++ src/model/tileset/TileMap.java
@@ -2,8 +2,27 @@ package model.tileset;
 
 public class TileMap {
 	
+	private int[][] tilemap;
+	private Tile[][] tiles;
 	public TileMap() {
 		
 	}
+	
+	public TileMap(int[][] map,int x, int y) {
+		tilemap = map;
+		tiles = new Tile[x][y];
+		loadTiles(x, y);
+	}
+	
+	public void loadTiles(int x, int y)
+	{
+		for (int i =0; i< x; i++)
+		{
+			for(int k=0 ; k<y; k++)
+			{
+				tiles[i][k] = new Tile(false,i*Tile.size,i*Tile.size,tilemap[i][k]);
+			}
+		}
+	}	
 	
 }
