3ae4f041823304b50fcbd45fa8322166cf748875
xrgman <xrgman@gmail.com>
2015-06-12 16:37:24 +0200

    fissaa

--- src/control/InputHandler.java
+++ src/control/InputHandler.java
@@ -1,5 +1,10 @@
 package control;
 
+/**
+ * Input handler from arduino and wii motes.
+ * @author 
+ * @version 1.3
+ */
 public class InputHandler {
 	
 	private ArduinoHandler arduino;
@@ -11,9 +16,14 @@ public class InputHandler {
 	private boolean pressurePlate3; //Right foot
 	private boolean pressurePlate4; //Left foot
 	
+	/**
+	 * Constructor.
+	 * @param arduinoCommPort - The arduino commport.
+	 */
 	public InputHandler(String arduinoCommPort)
 	{
 		//this.arduino = new ArduinoHandler(arduinoCommPort,this);
+		this.arduino = null;
 		this.wiimote = new WiiMoteHandler();
 		resetPressurePlates();
 	}
@@ -41,6 +51,14 @@ public class InputHandler {
 	public int getX2()
 	{
 		return wiimote.getP2aX();
+	}
+	
+	public boolean isA2Pressed() {
+		return wiimote.getA2Pressed();
+	}
+
+	public boolean isA1Pressed() {
+		return wiimote.getA1Pressed();
 	}
 	
 	//Adruino stuff:
@@ -141,29 +159,35 @@ public class InputHandler {
 				arduino.sendCommand("L1D");
 		}
 		
+		/**
+		 * The status of the first pressure plate.
+		 * @return - Status of pressure plate 1.
+		 */
 		public boolean getPressurePlate1() {
 			return pressurePlate1;
 		}
 		
+		/**
+		 * The status of the second pressure plate.
+		 * @return - Status of pressure plate 2.
+		 */
 		public boolean getPressurePlate2() {
 			return pressurePlate2;
 		}
 		
+		/**
+		 * The status of the thirth pressure plate.
+		 * @return - Status of pressure plate 3.
+		 */
 		public boolean getPressurePlate3() {
 			return pressurePlate3;
 		}
 		
+		/**
+		 * The status of the fourth pressure plate.
+		 * @return - Status of pressure plate 4.
+		 */
 		public boolean getPressurePlate4() {
 			return pressurePlate4;
-		}
-
-		public boolean isA2Pressed() {
-			return wiimote.getA2Pressed();
-		}
-
-		public boolean isA1Pressed() {
-			return wiimote.getA1Pressed();
-		}
-
-		
+		}		
 }
