e5a0465448043f1a96eff5f0cdcd8699e13f622b
kees2125 <kees2125@live.nl>
2015-06-20 17:33:10 +0200

    eind keuze toegevoegt in EndState

--- src/control/ScoreHandler.java
+++ src/control/ScoreHandler.java
@@ -6,5 +6,6 @@ public class ScoreHandler {
 	public int cityScore = 0;
 	public int bossScore = 0;
 	public boolean armGekozen = false;
+	public boolean geldGehouden = false;
 	
 }
--- src/model/gamestates/story/EndState.java
+++ src/model/gamestates/story/EndState.java
@@ -1,30 +1,59 @@
 package model.gamestates.story;
 
 import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.RenderingHints;
 import java.awt.event.KeyEvent;
+import java.awt.geom.AffineTransform;
 
-import control.ControlManager;
 import model.gamestates.GameState;
+import control.ControlManager;
+import control.ImageHandler;
+import control.ImageHandler.ImageType;
 
 public class EndState extends GameState {
-
-
+    private int scale = 1;
+    private int midX,midY,bgWidth,bgHeight;
+    private Image background;
 	
 	public EndState(ControlManager cm) {
 			super(cm);
-			
+			background = ImageHandler.getImage(ImageType.endChoice);
+			midX = ControlManager.screenWidth/2;
+			midY = ControlManager.screenHeight/2;
+			bgWidth = background.getWidth(null);
+			bgHeight = background.getHeight(null);
 		}
 	
 	
 	@Override
 	public void draw(Graphics2D g2) {
 		// TODO Auto-generated method stub
-		
+		AffineTransform tx = new AffineTransform();
+		AffineTransform bg = new AffineTransform();
+		tx.translate(midX, midY);
+		g2.setTransform(tx);
+		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		bg = tx;
+		bg.scale(scale, scale);
+		g2.setTransform(bg);
+		g2.drawImage(background, -bgWidth/2,-bgHeight/2,null);
 	}
 	
 	@Override
 	public void update() {
-		// TODO Auto-generated method stub
+		boolean pressurePlate1 = cm.getInputHandler().getPressurePlate1(); //Right foot
+		boolean pressurePlate2 = cm.getInputHandler().getPressurePlate2(); //Left foot
+		boolean pressurePlate3 = cm.getInputHandler().getPressurePlate3(); //Right foot
+		boolean pressurePlate4 = cm.getInputHandler().getPressurePlate4(); //Left foot
+		if(pressurePlate1 && pressurePlate3 && !pressurePlate2 && !pressurePlate4) { // Go to the rights
+			
+				cm.getGameStateManager().next();
+		}
+		else if(!pressurePlate1 && !pressurePlate3 && pressurePlate2 && pressurePlate4) {// Go to the left
+			cm.getScoreHandler().bossScore += 2500;
+			cm.getGameStateManager().next();
+		}
 		
 	}
 	
@@ -40,7 +69,15 @@ public class EndState extends GameState {
 	@Override
 	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+		if(e.getKeyCode() == KeyEvent.VK_RIGHT) { // Go to the rights
+			cm.getScoreHandler().geldGehouden = true;
+			cm.getGameStateManager().next();
+			
+		}
+		else if(e.getKeyCode() == KeyEvent.VK_LEFT) {// Go to the left
+			cm.getScoreHandler().geldGehouden = false;
+			cm.getGameStateManager().next();
+		}
 	}
 	
 	@Override
